13413dbc8a3645b34845fe9fe9d1ef67552c25b1
mandlerm <amaya.melissa@gmail.com>
2018-01-02 16:25:03 -0600

    user_books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180102054303) do
+ActiveRecord::Schema.define(version: 20180102222419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20180102054303) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user_books", force: :cascade do |t|
+    t.integer "book_id"
+    t.integer "user_id"
+    t.integer "rating"
+    t.string "comments"
+    t.boolean "read"
   end
 
   create_table "users", force: :cascade do |t|

337c4d2af5244ab391c7531bec5b1ce7c8f0dece
mandlerm <amaya.melissa@gmail.com>
2018-01-02 00:44:05 -0600

    strong params in book controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,4 +4,28 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    raise params.inspect
+  end
+
+
+  private
+    def books_params
+      params.require(:book).permit(:title, :author, :book_category =>
+                      {:rating, :comments}, :category => {:category})
+    end
+
 end
+
+#
+# "book"=>{
+#   "title"=>"Story of the World",
+#   "author"=>"Susan Bauer",
+#   "book_category"=>{
+#     "rating"=>"5",
+#     "comments"=>"great history book"
+#   },
+#   "category"=>{
+#     "category"=>"History"
+#   }
+# },

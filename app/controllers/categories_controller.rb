9b773d5ff645e300807834ca00d29d4c86501c3d
mandlerm <amaya.melissa@gmail.com>
2018-01-02 18:00:14 -0600

    reword of new view page in progess. moved form to category/new

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,13 @@
+class CategoriesController < ApplicationController
+
+  def new
+    @category = Category.new
+
+  end
+
+  def create
+
+  end
+
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def index
+    # sort books.
     @books = Book.all
+
+    # give option to view Alphabetically.  Only those read. Only those not read
+    # by category
   end
 
 end

d5483ada5482933b10c18efdba6e3c7cfe372b13
mandlerm <amaya.melissa@gmail.com>
2018-01-05 12:06:03 -0600

    add devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users,  :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :users, only: [:show]
+  resources :users do
+    resources :books
+  end
+
   resources :categories, only: [:new, :create, :show]
   resources :book_records, only: [:new, :create, :show]
-  resources :books, only: [:show, :new, :create, :edit, :update, :destroy]
+  # resources :books, only: [:show, :new, :create, :edit, :update, :destroy]
   devise_scope :user do
     root to: "devise/sessions#new"
   end
-
 end

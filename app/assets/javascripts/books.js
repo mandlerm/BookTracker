1a64c85ac1481335f974ae19f6c41a6ec5ab7c5b
mandlerm <amaya.melissa@gmail.com>
2018-02-17 20:28:39 -0600

    book show set up via ajax request

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -56,18 +56,10 @@ Book.prototype.formatIndex = function() {
       })
           .then(res => res.json())
           .then(data => {
-            console.log(data)})
-      //         $(".app-container").html('').append(`<h1>Your Books</h1><table><tbody class='book-list'><tr>
-      //                 <th class="table"> Title </th>
-      //                 <th class="table"> Author </th>
-      //                 <th class="table"> Category </th>
-      //                 </tbody></table>`)
-      //
-      //       data.books.forEach(book => {
-      //
-      //         $(".book-list").append(`<tr><td><a href='http://localhost:3000/${url}/books/${book.id}' class='book-show'>${book.title}</a></td><td>${book.author}</td><td>${book.category_name}</td>`)
-      //         console.log(book)
-      //       })
-      //   })
-      })
-  }
+            console.log(data)
+            let bookHTML = `
+              <h1>${data.title}</h1><p>Author:${data.author}</p>
+            `
+             $(".app-container").html('').append(bookHTML)
+          })
+  })}
